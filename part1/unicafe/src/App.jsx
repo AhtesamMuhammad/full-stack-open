615e5cb10c43cc133322381ecd199155f0261f20
Ahtesam <137303420+AhtesamMuhammad@users.noreply.github.com>
2024-06-18 11:55:32 +0200

    refactor name and 1.3

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,51 +1,59 @@
-const Header = (course) => {
-  return <p>{course.name}</p>;
+const Header = (propos) => {
+  return <p>{propos.name}</p>;
 };
 
-const Part = (lesson) => {
+const Part = (props) => {
   return (
     <>
-      part: {lesson.part} and exercises: {lesson.exercises}
+      part: {props.part} and exercises: {props.exercises}
     </>
   );
 };
 
-const Content = (lessons) => {
+const Content = (props) => {
   return (
     <div>
-      <Part part={lessons.part[0]} exercises={lessons.exercises[0]} />
+      <Part part={props.part[0]} exercises={props.exercises[0]} />
       <br />
-      <Part part={lessons.part[1]} exercises={lessons.exercises[1]} />
+      <Part part={props.part[1]} exercises={props.exercises[1]} />
       <br />
-      <Part part={lessons.part[2]} exercises={lessons.exercises[2]} />
+      <Part part={props.part[2]} exercises={props.exercises[2]} />
     </div>
   );
 };
-const Total = ({ number }) => {
+const Total = ({ numbers }) => {
   let total = 0;
-  for (let index = 0; index < number.length; index++) {
-    total += number[index];
+  for (let index = 0; index < numbers.length; index++) {
+    total += numbers[index];
   }
   return <p>Total exercises: {total}</p>;
 };
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10 
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+  
 
   return (
     <div>
       <Header name={course} />
       <Content
-        part={[part1, part2, part3]}
-        exercises={[exercises1, exercises2, exercises3]}
+        part={[part1.name, part2.name, part3.name]}
+        exercises={[part1.exercises, part2.exercises, part3.exercises]}
       />
-      <Total number={[exercises1, exercises2, exercises3]} />
+      <Total numbers={[part1.exercises, part2.exercises, part3.exercises]} />
     </div>
   );
 };
