a95f44aa5c529dc25b88c2f8c0612595014adcb8
Ahtesam <137303420+AhtesamMuhammad@users.noreply.github.com>
2024-06-28 13:44:48 +0200

    activity 1.7 done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,22 +4,56 @@ const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>{text}</button>
 );
 
+const Average = ({arrayClicks}) => {
+  if(arrayClicks.length === 0){
+    return(
+      <div>0</div>
+    )
+  }
+
+  const sum = arrayClicks.reduce((sum, number) => sum + number,0)
+  const average = sum / arrayClicks.length
+  return(
+    <div>
+      {average}
+    </div>
+  )
+}
+
+const Positive = ({arrayClicks}) => {
+  if(arrayClicks.length === 0){
+    return(
+      <div>0 %</div>
+    )
+  }
+  const sumPositive = arrayClicks.filter(value => value > 0).length
+  const percent = (sumPositive/arrayClicks.length)*100
+  return (
+    <div>{percent} %</div>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [allClicks, setAll] = useState([]);
 
   const handleGoodClick = () => {
     setGood(good + 1);
+    setAll(allClicks.concat(1));
   };
 
   const handleNeutralClick = () => {
     setNeutral(neutral + 1);
+    setAll(allClicks.concat(0));
   };
 
   const handleBadClick = () => {
     setBad(bad + 1);
+    setAll(allClicks.concat(-1));
   };
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -30,6 +64,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {allClicks.length}</p>
+      <Average arrayClicks={allClicks}/>
+      <Positive arrayClicks={allClicks}/>
     </div>
   );
 };
